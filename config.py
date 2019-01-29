b36484a9e3e6e8286bbd4840b219dff6f3262a61
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2019-01-30 02:24:08 +0900

    テストコード

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = 't6GXzOVRnjV3BqGnxWEFokTtk'
+consumer_secret = '4n9Nn4bmUFfcRYZOguOwpWUZZiSDPiEAvejZsxvIPB4dk9I2rO'
+access_token = '4n9Nn4bmUFfcRYZOguOwpWUZZiSDPiEAvejZsxvIPB4dk9I2rO'
+access_token_secret = '4n9Nn4bmUFfcRYZOguOwpWUZZiSDPiEAvejZsxvIPB4dk9I2rO'
+
