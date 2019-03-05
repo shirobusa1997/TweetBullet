3dc4c7083e68caac0ff11c361dcb5c8f001f03fa
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2019-03-05 11:58:42 +0900

    トークンを更新しました。 コミットの各種キーは、すでに破棄されています。

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-consumer_key = 't6GXzOVRnjV3BqGnxWEFokTtk'
-consumer_secret = '4n9Nn4bmUFfcRYZOguOwpWUZZiSDPiEAvejZsxvIPB4dk9I2rO'
-access_token = '4n9Nn4bmUFfcRYZOguOwpWUZZiSDPiEAvejZsxvIPB4dk9I2rO'
-access_token_secret = '4n9Nn4bmUFfcRYZOguOwpWUZZiSDPiEAvejZsxvIPB4dk9I2rO'
+consumer_key = 'VQWvbOsK6rKV72aD5jQQKOOCS'
+consumer_secret = 'hhCx6ByWH5Hj74psJvV4WWwsHOpYbzBQBf9VYmN181DrKFM7Zr'
+access_token = '3179072406-j3k0f3xoYPNyt2jCeQlgZcEcoLFIwj7x3otkTS5'
+access_token_secret = '75yh7PsBpbboikAqGqBxUz5CVzF46bdTFuxpvxLSds2hu'
 
