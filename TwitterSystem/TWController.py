9d999849b98fc9ac1d5ab7b093cbb6dd09d41b78
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2019-03-15 03:55:06 +0900

    TwitterAPIを叩く処理群を別パッケージとして集約。

--- TwitterSystem/TWController.py
+++ TwitterSystem/TWController.py
@@ -20,20 +20,27 @@ class TWController(Object):
 	TW_USER_name = "[UserName]"
 	TW_USER_id	 = "[UserID]"
 
-	CONSUMER_KEY 		= "Unknown"
-	CONSUMER_SECRET 	= "Unknown"
-	ACCESS_TOKEN 		= "Unknown"
-	ACCESS_TOKEN_SECRET = "Unknown"
-	TWITTER_OAUTH 		= "Unknown"
+	CONSUMER_KEY 		= 'Unknown'
+	CONSUMER_SECRET 	= 'Unknown'
+	ACCESS_TOKEN 		= 'Unknown'
+	ACCESS_TOKEN_SECRET = 'Unknown'
+	TWITTER_OAUTH 		= 'Unknown'
 
 	RESOURCE_URL = "https://api.twitter.com/1.1/statuses/update.json"
 
+	# OAuthによるユーザ認証
 	def authorize_user():
 		CONSUMER_KEY 		= config.consumer_key
 		CONSUMER_SECRET 	= config.consumer_secret
 		ACCESS_TOKEN 		= config.access_token
 		ACCESS_TOKEN_SECRET = config.access_token_secret
+		# OAuthによる認証処理試行
 		try:
-			TWITTER_OAUTH 	= OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+			TWITTER_OAUTH = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+		# すべての例外をキャッチ
 		except Exception as e:
-			raise e
+			print(e)
+			return False
+		else :
+			print("Authorization Completed.")
+			return True
