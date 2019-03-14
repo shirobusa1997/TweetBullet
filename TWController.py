dc51cdf737e5b556a7197826852bc8e72e3bff40
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2019-03-15 03:02:56 +0900

    TwitterAPIを叩くクラスの宣言と機能分割の準備。 初期化メソッドの追加。

--- TWController.py
+++ TWController.py
@@ -1,13 +1,8 @@
+# コンフィグデータ参照
 import Configulations.config as config, json
+
+# 標準モジュール参照
 from requests_oauthlib import OAuth1Session
-
-CONSUMER_KEY = config.consumer_key
-CONSUMER_SECRET = config.consumer_secret
-ACCESS_TOKEN = config.access_token
-ACCESS_TOKEN_SECRET = config.access_token_secret
-TWITTER_OAUTH = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-RESOURCE_URL = "https://api.twitter.com/1.1/statuses/update.json"
 
 print("Please input details of your posts.")
 tweet = input('>> ')
@@ -20,3 +15,25 @@ if res.status_code == 200:
 	print("Comfirmed your post.")
 else:
 	print("Failed : %d"% res.status_code)
+
+class TWController(Object):
+	TW_USER_name = "[UserName]"
+	TW_USER_id	 = "[UserID]"
+
+	CONSUMER_KEY 		= "Unknown"
+	CONSUMER_SECRET 	= "Unknown"
+	ACCESS_TOKEN 		= "Unknown"
+	ACCESS_TOKEN_SECRET = "Unknown"
+	TWITTER_OAUTH 		= "Unknown"
+
+	RESOURCE_URL = "https://api.twitter.com/1.1/statuses/update.json"
+
+	def authorize_user():
+		CONSUMER_KEY 		= config.consumer_key
+		CONSUMER_SECRET 	= config.consumer_secret
+		ACCESS_TOKEN 		= config.access_token
+		ACCESS_TOKEN_SECRET = config.access_token_secret
+		try:
+			TWITTER_OAUTH 	= OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+		except Exception as e:
+			raise e
